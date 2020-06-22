2ad400db4bc4bbf5041502374cb627fcd36c2837
manhoosbilli1 <shoaib.mustafa7@hotmail.com>
2020-06-22 13:30:50 +0100

    final

--- ultrasonic-test.py
+++ ultrasonic-test.py
@@ -1,19 +1,20 @@
+#! /usr/bin/env python
+
 import RPi.GPIO as GPIO
 import time
 import os
 import serial
-
-
+print("Program started...")
 
     
 try:
     GPIO.setmode(GPIO.BCM)  #settings gpios as per BCM convention
-    ser = serial.Serial('/dev/ttyUSB0', 9600)   #setup serial
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)   #setup serial
+    
     trig = 23
     echo = 24
     moisture = 22
     flame = 27
-    shutdown_btn = 6
 
     GPIO.setup(trig, GPIO.OUT)
     GPIO.output(trig, 0)
@@ -49,36 +50,29 @@ try:
     
     
     while True:
+        ser.flush()
         time.sleep(0.05)  #settling delay. 
-        
         distance = ultrasonic()
         
-        #when shutting down
-        #if(GPIO.input(shutdown_btn) == 0):  #when the shutdown button is grounded
-        #   os.system('mpg321 shutting-down.mp3')
-        #    time.sleep(1)
-        #    os.system('sudo shutdown now -h')
             
+        
+        
         #flame detection
         if(GPIO.input(flame) == 0):
-            os.system('mpg321 Fire-detected.mp3')
+            os.system('omxplayer /home/pi/Desktop/FYP\ /flame-detected.mp3')
           
         #moisture detection
         elif(GPIO.input(moisture) == 0):                   #moisture detection 
-            os.system('mpg321 moisture-detected.mp3')    #plays sound
+            os.system('omxplayer /home/pi/Desktop/FYP\ /moisture-detected.mp3')    #plays sound
         
         #obstacle detection
         elif(distance > 8 and distance <12):
-            os.system('mpg321 sound.mp3')
+            os.system('omxplayer /home/pi/Desktop/FYP\ /sound.mp3')
         
         #pulse detection
-        if(ser.in_waiting>0):             #pulse detection
-            pulse = ser.readline()
-            pulse = int(pulse.strip())
-            if(pulse >50 and pulse < 160):
-                #os.system('mpg321 BPM-high.mp3')
-                print("pulse detected, {}".format(pulse))
-            
+        if ser.in_waiting > 0:             #pulse detection
+            pulse = int(ser.readline().decode('utf-8').strip())
+            print(pulse)
             
             
 except KeyboardInterrupt:
